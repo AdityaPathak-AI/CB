0d6ff6ccb595bbaddce2bc525f95084fdb95d8cd
Aditya Pathak <sumitpathak861@gmail.com>
2023-09-13 15:25:05 +0530

    we back

--- JS/Javascript/interest.js
+++ JS/Javascript/interest.js
@@ -10,3 +10,42 @@ function SimpleInterest() {
   document.getElementById("SI").innerHTML= "Your Simple Interest Is:"+ SimpleInt;
   document.getElementById("TotalAmount").innerHTML=  "Your Total Amount Is:"+Amount;
 }
+
+
+// #swapping without operators.
+// a=a+b;
+// b=a-b;
+// a=a-b;
+
+// document.write(10/3+"<br>")
+// document.write(10%3+"<br>")
+// document.write(parseInt(10/3+"<br>"));
+// document.write(math.floor(10/3+"<br>"));
+
+
+// var a1,a2,a3,b1,b2,b3,sum,rev;
+// var n =parseInt(prompt("enter any 4 digit number")); //1234
+// a1=Math.floor(n/1000); //1
+// a2=n%1000; 234
+// b1=Math.floor(b1/100); //2
+// b2=b1%100; //34 
+// a3=Math.floor(b2/10);3
+// b3=b2%10; //4
+// sum=a1+a2+a3+b3;
+// rev=(b3*1000)+(a3*100)+(a2*10)+a1;
+// document.write("Sum:" + sum + "<br>");
+// document.write("reverse"+ rev +"<br>");
+
+
+// number of days into week , days & years
+
+
+// function Area(){
+//   var radius = parseFloat(document.getElementById("radius").value);
+//   var area = Math.PI*radius*radius;
+
+//   document.getElementById("result").innerHTML="ares is:"
+// }
+
+// 12345
+// 14325
--- JS/Javascript/pattern.js
+++ JS/Javascript/pattern.js
@@ -1,5 +1,13 @@
-document.write("*" + "<br>");
-document.write("**"+ "<br>");
-document.write("***"+ "<br>");
-document.write("****"+ "<br>");
-document.write("*****");
+// document.write("*" + "<br>");
+// document.write("**"+ "<br>");
+// document.write("***"+ "<br>");
+// document.write("****"+ "<br>");
+// document.write("*****");
+
+for (let i = 1; i <= 5; i++) {
+  r = " ";
+  for (let j = 1; j <= i; j++) {
+    r += " * ";
+  }
+  console.log(r);
+}
