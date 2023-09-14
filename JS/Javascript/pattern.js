7934dd52c010a9f28c8037006432270832af2e80
Aditya Pathak <sumitpathak861@gmail.com>
2023-09-14 18:04:30 +0530

    js

--- JS/Javascript/pattern.js
+++ JS/Javascript/pattern.js
@@ -4,10 +4,3 @@
 // document.write("****"+ "<br>");
 // document.write("*****");
 
-for (let i = 1; i <= 5; i++) {
-  r = " ";
-  for (let j = 1; j <= i; j++) {
-    r += " * ";
-  }
-  console.log(r);
-}
