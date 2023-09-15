e0236cba9b2544c5bb2fab718e04826f83a8136a
Aditya Pathak <sumitpathak861@gmail.com>
2023-09-15 18:08:34 +0530

    .

--- JS/Javascript/increment.js
+++ JS/Javascript/increment.js
@@ -1,5 +1,68 @@
 // x++ + ++x + ++x - --x + --x
 // 10+12+13-12+11
 
+// let a = 1;
+// a+=5 
+// console.log(a)
+// a-=1
+// console.log(a)
+// a*=5
+// console.log(a)
+// a/=5
+// console.log(a)
+
+// 1
+// Num = +(prompt("Enter Number to check"));
+// if(Num%2==0)
+// document.write("Your Number is Even")
+// else
+// document.write("Your Number is odd")
+
+
+// 2
+// Num1= +(prompt("enter first number"));
+// Num2= +(prompt("enter first number"));
+
+// if(Num1%Num2==0)
+// document.write("Is Divisible")
+// else
+// document.write("not divsible")
+
+
+// 3
+// Num1= +(prompt("enter first number"));
+// Num2= +(prompt("enter second number"));
+// Num3= +(prompt("enter third number"));
+
+// if(Num1>Num2 && Num1>Num3)
+// document.write("First is greater");
+// else 
+// if(Num2>Num3)
+// document.write("Second is greater");
+// if(Num1==Num2 && Num1==Num3)
+// document.write("All numbers are equal");
+// else
+// document.write("Enter Valid Number");
+
+
+// var character = prompt("Enter a character:");
+
+// if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) 
+// {
+//   if (character === character.toLowerCase())
+//    {
+//     document.write(character + " is in lowercase.");
+//    } 
+//   else 
+//    {
+//     document.write(character + " is in uppercase.");
+//    }
+// } 
+
+
+
+
+
+
 
 
--- JS/Javascript/tempCodeRunnerFile.js
+++ JS/Javascript/tempCodeRunnerFile.js
@@ -1,1 +1,2 @@
-console.log(++a);
+a+=1
+console.log(a)
