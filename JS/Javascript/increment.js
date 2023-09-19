b4cb10385d3b40f548b2ef471f80c446d88b9984
Aditya Pathak <sumitpathak861@gmail.com>
2023-09-19 16:41:44 +0530

    Loops

--- JS/Javascript/increment.js
+++ JS/Javascript/increment.js
@@ -127,3 +127,48 @@
 
 // }
 
+// var a = +(prompt("Enter first number:"))
+// var b = +(prompt("Enter second number:"))
+
+
+// var operation = +prompt("Choose operation from \n 1)+  \n 2)-  \n 3)*  \n 4)/ ");
+// var result
+
+// switch(operation){
+//     case 1:
+//         result=a+b;
+//         document.write(result)
+//         break;
+
+//     case 2:
+//         result = a-b;
+//         document.write(result)
+//         break;
+
+//     case 3:
+//         result = a*b;
+//         document.write(result)
+//         break;
+
+//     case 4:
+//         result = a/b;
+//         document.write(result)
+//         break;
+        
+//     default:
+//         document.write("Inavlid Keys");
+
+// }
+
+
+var i = 1 ,sum=0;
+
+while(i<=10)
+{
+document.write(i*i*i+"<br>");
+sum=sum+i;  
+i=i+1;
+}
+document.write(sum); 
+
+
