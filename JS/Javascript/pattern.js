b91bb3669b62de11538d183ac8522747aedda6e4
Aditya Pathak <sumitpathak861@gmail.com>
2023-10-14 17:03:26 +0530

    .

--- JS/Javascript/pattern.js
+++
@@ -1,6 +0,0 @@
-// document.write("*" + "<br>");
-// document.write("**"+ "<br>");
-// document.write("***"+ "<br>");
-// document.write("****"+ "<br>");
-// document.write("*****");
-
--- Javascript/Events/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-function modifyText() {
-//   const t2 = document.getElementById("t2");
-//   const isNodeThree = t2.firstChild.nodeValue === "three";
-//   t2.firstChild.nodeValue = isNodeThree ? "two" : "three";
-// }
-
-
-// const el = document.getElementById("outside");
-// el.addEventListener("click", modifyText, false);
