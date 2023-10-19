cdd8ba600602dd41297a596d60798b5d9b518ea7
Aditya Pathak <sumitpathak861@gmail.com>
2023-10-19 21:13:54 +0530

    BULB

---
+++ Javascript/10-DOM/01-4 Pillars/app.js
@@ -0,0 +1,7 @@
+var a = document.querySelector("h1")
+a.addEventListener("click" , function(){
+     a.innerHTML= "Russian Federation"
+    a.style.color="Orange"
+    a.style.backgroundColor= "#000";
+})
+
