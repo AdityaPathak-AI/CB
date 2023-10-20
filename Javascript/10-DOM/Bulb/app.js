67271cbd731363a60753c9a30881e2f2cb1ddd14
Aditya Pathak <sumitpathak861@gmail.com>
2023-10-20 16:24:37 +0530

    lessgoo.

--- Javascript/10-DOM/Bulb/app.js
+++ Javascript/10-DOM/Bulb/app.js
@@ -1,7 +1,8 @@
 var bulb = document.querySelector("#bulb")
-var btn  = document.querySelector("button")
-var flag = 0
+var btn1 = document.querySelector("#On")
+var btn2 = document.querySelector("#Off") 
 
+/* var flag = 0
 btn.addEventListener("click", function(){
     if(flag == 0)
     {
@@ -10,8 +11,14 @@ btn.addEventListener("click", function(){
     }
     else
     {
-    bulb.style.backgroundColor = "black";
+    bulb.style.backgroundColor = "b";
     flag = 1;
     }     
-})
+}) */
 
+btn1.addEventListener("click",function(){
+    bulb.style.backgroundColor = "yellow";
+})
+btn2.addEventListener("click", function(){
+    bulb.style.backgroundColor = "black";
+})
