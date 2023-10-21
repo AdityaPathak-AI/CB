ca53ca565dce0a6a59d7fe5caeb87106e65dd4d9
Aditya Pathak <sumitpathak861@gmail.com>
2023-10-21 17:32:27 +0530

    .

--- Javascript/10-DOM/Bulb/app.js
+++ Javascript/10-DOM/Bulb/app.js
@@ -1,24 +1,30 @@
 var bulb = document.querySelector("#bulb")
+
+// var btn = document.querySelector("#On")
+
 var btn1 = document.querySelector("#On")
 var btn2 = document.querySelector("#Off") 
 
-/* var flag = 0
-btn.addEventListener("click", function(){
-    if(flag == 0)
-    {
-    bulb.style.backgroundColor = "yellow";
-    flag = 1
-    }
-    else
-    {
-    bulb.style.backgroundColor = "b";
-    flag = 1;
-    }     
-}) */
+
+// var flag = 0
+// btn.addEventListener("click", function(){
+//     if(flag == 0)
+//     {
+//     bulb.style.backgroundColor = "yellow";
+//     flag = 1
+//     }
+//     else
+//     {
+//     bulb.style.backgroundColor = "black";
+//     flag = 0;
+//     }  
+//     
+// })
 
 btn1.addEventListener("click",function(){
     bulb.style.backgroundColor = "yellow";
 })
+
 btn2.addEventListener("click", function(){
     bulb.style.backgroundColor = "black";
 })
