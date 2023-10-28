86b935a529fe675bdf2103fd48fca340f4e73373
Aditya Pathak <sumitpathak861@gmail.com>
2023-10-28 15:54:35 +0530

    KRC

--- Javascript/05-Array's Built in Functions/forEach().js
+++ Javascript/05-Array's Built in Functions/forEach().js
@@ -1,16 +1,28 @@
 // Call a function for each Element.
-
+/*
 const a = [1, -2, 3, -4, 5, -6, 7, -8, 9, 10];
 const neg = (num)=>{
     return num <0;
 }
 a.forEach((num)=>{
-    if(num<0)
+    num<0;
     console.log(a)
-
 })
 
-// a.forEach((num,i)=>
-// {
-// console.log(num,i);
-// });
+a.forEach((num,i)=>
+{
+console.log(num,i);
+});
+Find the EVEN numbers in an array
+*/
+
+/* Negative */
+
+const arr = [1, -2, 3, -4, 5, 6, 7, -8, 9];
+const neg = [];
+arr.forEach(number => {
+  if (number < 0) {
+    neg.push(number);
+  }
+});x
+console.log(neg);
