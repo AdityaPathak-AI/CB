27e23db9779fb57ab219ae32a8408a510f5cee15
Aditya Pathak <sumitpathak861@gmail.com>
2023-10-30 20:22:31 +0530

    responsive navbar completed.

--- responsive/app.js
+++ responsive/app.js
@@ -3,4 +3,11 @@ const ul = document.querySelector('ul');
 
 hamburger.addEventListener('click', ()=>{
     ul.classList.toggle('ham_active');
+    
+    let ham_child = hamburger.firstElementChild;
+    if(ul.classList.contains('ham_active')){
+        ham_child.classList.replace('fa-bars','fa-xmark');
+    }else{
+        ham_child.classList.replace('fa-xmark','fa-bars')
+    }
 });
