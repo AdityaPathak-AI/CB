12bcffbfc8bff4128695066144d6ec7a40f5ec63
Aditya Pathak <sumitpathak861@gmail.com>
2023-11-06 21:15:56 +0530

    Grind Week.

--- Javascript/10-DOM/01.Query Selector/app.js
+++
@@ -1,7 +0,0 @@
-var a = document.querySelector("h1")
-a.addEventListener("click" , function(){
-     a.innerHTML= "Russian Federation"
-    a.style.color="Orange"
-    a.style.backgroundColor= "#000";
-})
-
---
+++ Javascript/Advanced Functions/need2know.js
@@ -0,0 +1,55 @@
+//1.Advanced If Else
+
+/*
+
+let age = 16;
+let name = age>20 ? "Pedro":"Jack"
+console.log(name);
+
+
+let age=16;
+const component = () =>{
+    return age > 10? "Hola Amigo":"Wassup"
+};
+console.log(component());
+
+*/
+
+
+/*
+//2. Dealing with objects.  (Destructing Property)
+
+const Person ={
+    Name : "Aditya",
+    age : 21,
+    isMarried : false,
+}
+
+const {Name , age , isMarried} = Person;
+console.log(Person);
+*/
+
+
+/*
+// 3. Spread Operator
+// 3.1 Copy another object with slight changes.(Spread Operator)
+
+const Person2 = {...Person, Name:"Sumit"}
+console.log(Person2);
+
+// 3.2 Addding data to array using spread opeator
+const names = ["aditya" , "sumit" , "krishna"];
+const names2 = [...names , "gaurav"]
+console.log(names);
+console.log(names2);
+*/
+
+// filter()
+ 
+let names = ["jessica", "Carol" , "nigga", "nigga", "homie", "sumit"];
+let updated = names.filter((name)=>{
+    return name !== "nigga"
+    
+})
+console.log(updated);
+
