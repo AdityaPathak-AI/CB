45cd30571c6a9c67458df71d157c5b83940130cc
Aditya Pathak <sumitpathak861@gmail.com>
2023-12-09 20:19:03 +0530

    CRUD

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main">
+      <h2 className="main-header">React Crud Operations</h2>
+    </div>
+  );
+}
+
+export default App;
